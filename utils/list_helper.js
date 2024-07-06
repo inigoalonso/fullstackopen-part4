02b992994c9b7e778f006ee5a7786945eebf9957
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-06 17:50:00 +0200

    4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,6 +14,28 @@ const favoriteBlog = (blogs) => {
   }, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const authors = blogs.reduce((authors, blog) => {
+    if (authors[blog.author]) {
+      authors[blog.author]++
+    } else {
+      authors[blog.author] = 1
+    }
+    return authors
+  }, {})
+
+  const author = Object.keys(authors).reduce((author, current) => {
+    return (authors[current] > authors[author]) ? current : author
+  }, Object.keys(authors)[0])
+
+  return {
+    author: author,
+    blogs: authors[author]
+  }
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs
 }
