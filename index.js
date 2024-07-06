9e2d3fe56881eca5667e851a1957388d3443b669
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-06 10:39:13 +0200

    4.1

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json())
 
 // Define the URL for the MongoDB database
 const url = process.env.MONGODB_URI
@@ -34,15 +36,36 @@ app.get('/api/blogs', (request, response) => {
     })
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+app.post('/api/blogs', (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
+    console.log('blog:', blog)
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+      .catch((error) => next(error))
+  } catch (error) {
+    next(error) // Forward to error handling middleware
+  }
 })
+
+// Error handler middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
