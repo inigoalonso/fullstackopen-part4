f4604477f81e30ba87da65cf4500f865bb5e3245
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-06 11:17:17 +0200

    4.2

---
+++ app.js
@@ -0,0 +1,72 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
+
+// const Blog = mongoose.model('Blog', blogSchema)
+
+// Define the URL for the MongoDB database
+const url = config.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', url)
+
+mongoose.connect(url)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
+
+// app.post('/api/blogs', (request, response, next) => {
+//   try {
+//     const blog = new Blog(request.body)
+//     console.log('blog:', blog)
+//     blog
+//       .save()
+//       .then(result => {
+//         response.status(201).json(result)
+//       })
+//       .catch((error) => next(error))
+//   } catch (error) {
+//     next(error) // Forward to error handling middleware
+//   }
+// })
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,73 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json())
-
-// Define the URL for the MongoDB database
-const url = process.env.MONGODB_URI
-// const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(url)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response, next) => {
-  try {
-    const blog = new Blog(request.body)
-    console.log('blog:', blog)
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-      .catch((error) => next(error))
-  } catch (error) {
-    next(error) // Forward to error handling middleware
-  }
-})
-
-// Error handler middleware
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-// this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
